cf008c70909a3d8356502b2c7743976f4047bf39
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-03-09 08:28:15 +0430

    add and use toaster and AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import AppRouter from './routes/appRouter';
+import {Toaster} from 'react-hot-toast';
 
 function App() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      
-      <main className="flex-grow flex items-center justify-center bg-gray-50">
-        <div className="text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900">Project Initialized</h2>
-          <p className="mt-2 text-gray-600">The architecture and styling engine are ready for development.</p>
-        </div>
-      </main>
 
-      <Footer />
+  return (
+    <div className="flex flex-col h-auto">
+      <Toaster position="top-center" reverseOrder={false} containerStyle={{zIndex:9999}}/>
+        <AppRouter/>
     </div>
   );
 }

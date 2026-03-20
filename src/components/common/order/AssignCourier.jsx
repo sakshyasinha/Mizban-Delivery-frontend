a9d7bc669adbffb0bd4ee92672b90e6ea489ba80
Shukria <shukriasul5@gmail.com>
2026-03-20 23:43:26 +0430

    fix: add useLockBodyScroll hook to prevent the body from scrolling when the modals appear

--- src/components/common/order/AssignCourier.jsx
+++ src/components/common/order/AssignCourier.jsx
@@ -3,6 +3,7 @@ import SearchableDropdown from "../SearchableDropdown";
 import useOrderStore from "../../../store/admin/useOrderStore";
 import toast from "react-hot-toast";
 import { X } from "lucide-react";
+import { useLockBodyScroll } from "../../../hooks/useLockBodyScroll";
 
 export default function AssignCourier({ onClose, isOpen, orderId }) {
   const selectedCourier = useOrderStore((state) => state.selectedCourier);
@@ -33,10 +34,10 @@ export default function AssignCourier({ onClose, isOpen, orderId }) {
       { id: 4, name: "Hassan", value: "hassan" },
       { id: 5, name: "Hussain", value: "hussain" },
     ];
-
+    useLockBodyScroll(isOpen)
   return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center p-4">
-      <div className="absolute inset-0 bg-gray-900/40 backdrop-blur-sm" onClick={handleCancel} />
+    <div className="fixed  overflow-hidden inset-0 z-[9999] flex items-center justify-center p-4">
+      <div className="absolute  inset-0 bg-gray-900/40 backdrop-blur-sm" onClick={handleCancel} />
       <div className="bg-white w-full max-w-md rounded-[24px] shadow-xl z-10">
         <div className="p-6 flex w-full flex-col items-start gap-4">
           <button  className="self-end hover:bg-orange-600 hover:text-white p-2 cursor-pointer rounded-[24px] transition ease-out" onClick={onClose}>

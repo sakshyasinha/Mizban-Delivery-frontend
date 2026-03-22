9056f3ec077809425ec3be0f16979e9ab3c3af71
fataneh-mow <fataneh.mow@gmail.com>
2026-03-23 00:46:06 +0430

    feat: composed Sidebar within the AppLayout.jsx #31

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,19 +1,24 @@
-import { Outlet } from "react-router-dom"
-import Header from "../components/common/Header"
-import Footer from "../components/common/Footer"
+import { Outlet } from "react-router-dom";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import Sidebar from "../components/common/Sidebar";
 
-export default function AppLayout () {
-    return (
-        <div className="min-h-screen flex flex-col bg-gray-100">
-            <Header />
+export default function AppLayout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <Header />
 
-            <main className="flex-1 w-full">
-                <div className="max-w-7xl mx-auto p-6">
-                    <Outlet />
-                </div>
-            </main>
+      <main className="flex flex-1 w-full flex-col md:flex-row">
+        {/* Sidebar */}
+        <Sidebar />
 
-            <Footer />
+        {/* Main content */}
+        <div className="flex-1 max-w-7xl mx-auto p-6">
+          <Outlet />
         </div>
-    )
+      </main>
+
+      <Footer />
+    </div>
+  );
 }

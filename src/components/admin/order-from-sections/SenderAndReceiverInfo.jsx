22321ed68d50a660fcde416ccb6b560923173f0c
Shukria <shukriasul5@gmail.com>
2026-03-23 01:14:39 +0430

    feat: add basic structure for SenderAndReceiver component

---
+++ src/components/admin/order-from-sections/SenderAndReceiverInfo.jsx
@@ -0,0 +1,20 @@
+export default function SenderAndReceiverInfo(){
+    <div>
+        <div>
+            <h2>Business Details</h2>
+             <label htmlFor="businessName">Business name</label>
+              <input type="text" id="businessName" placeholder="Shahmama Resturant"/>
+             <label htmlFor="businessPhoneNumber"></label>
+             <input type="text" id="businessPhoneNumber" placeholder="070000000"/>
+        </div>
+        <div>
+            <h2>Cusomer Details</h2>
+             <label htmlFor="customerName">Name</label>
+              <input type="text" id="customerName" placeholder="Ali Ahmadi"/>
+             <label htmlFor="customerPhoneNumber">Phone number</label>
+             <input type="text" id="customerPhoneNumber" placeholder="070000000"/>
+             <label htmlFor="customerAddress">Address</label>
+             <input type="text" id="customerAddress" placeholder="Apartment 4, Darulaman Road, District 6, Kabul"/>
+        </div>
+    </div>
+}

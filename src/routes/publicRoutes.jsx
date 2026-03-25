6681ebdf398516870009e02418766c1732c1a9f7
fataneh-mow <fataneh.mow@gmail.com>
2026-03-25 13:04:02 +0430

    feat: added new routes to the appR

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,10 +1,5 @@
 import Signup from '../pages/public/Signup';
-import Home from '../pages/public/Home';
 const publicRoutes=[
-    {
-        path:"/",
-        element:<Home/>
-    },
     {
         path:"/signup",
         element:<Signup/>

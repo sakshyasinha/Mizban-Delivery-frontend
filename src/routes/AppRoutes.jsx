8307c1c8dbc69e1ddac0fbc8eb6e43dcaaf4692c
fataneh-mow <fataneh.mow@gmail.com>
2026-03-25 13:01:38 +0430

    feat: implemented the auth and app layouts with their related routes #31

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,50 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-
-import CourierList from "../pages/admin/CourierList";
-import AddCourier from "../pages/admin/AddCourier";
-import EditCourier from "../pages/admin/EditCourier";
-import Orders from "../pages/admin/Orders";
-import OrderForm from "../components/admin/OrderForm";
+import publicRoutes from "./publicRoutes";
+import protectedRoutes from "./protectedRoutes";
 import AppLayout from "../layout/AppLayout";
+import AuthLayout from "../layout/AuthLayout";
 
-export default function AppRoutes () {
-    return (
-        <Routes>
-            <Route path="/" element={<AppLayout />}>
-                <Route index element={
-                    <div className="text-center mt-10">
-                        <h2 className="text-3xl font-extrabold text-gray-900">
-                            Project Initialized
-                        </h2>
-                        <p className="mt-2 text-gray-600">
-                            The architecture and styling engine are ready.
-                        </p>
-                    </div>
-                } />
+function AppRoutes() {
+  return (
+    <Routes>
 
-                <Route path="couriers" element={<CourierList />} />
-                <Route path="couriers/add" element={<AddCourier />} />
-                <Route path="couriers/edit/:id" element={<EditCourier />} />
+      {/* auth pages */}
+      <Route element={<AuthLayout />}>
+        {publicRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Route>
 
-                <Route path="orders" element={<Orders />} />
-                <Route path="order/create-order" element={<OrderForm />} />
-                <Route path="orders/edit-order/:id" element={<OrderForm />} />
-                <Route path="orders/view-order/:id" element={<OrderForm readOnly={true} />} />
+      {/* app pages */}
+      <Route path="/" element={<AppLayout />}>
+        {protectedRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Route>
 
-                <Route path="/deliveries" element={<h1>This is deliveries page</h1>} />
-
-                <Route path="settings" element={
-                    <div className="my-24 text-center">
-                        This is the settings page, settings is not accessible right now
-                    </div>
-                } />
-
-                <Route path="/analytics" element={<h1>This is analytics page</h1>} />
-                <Route path="/menu-manager" element={<h1>This is Menu manger's page</h1>} />
-
-                
-                <Route path="*" element={<h1>Page not Found</h1>} />
-            </Route>
-        </Routes>
-    )
+    </Routes>
+  );
 }
+
+export default AppRoutes;

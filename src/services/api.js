0d41854f1bcaae12652b78090f9d9c73c4304879
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-03-28 02:01:44 +0430

    refactore api serivce with Ky package

--- src/services/api.js
+++ src/services/api.js
@@ -1,40 +1,36 @@
-import axios from 'axios';
+import ky from 'ky';
 
-const api=axios.create({
-    baseURL:"https://mizban-delivery-backend.onrender.com/",
+const api=ky.create({
+    prefixUrl:"https://mizban-delivery-backend.onrender.com/",
     headers:{
         'Content-Type':"application/json"
     },
     timeout:30000, // 30 second timout
-});
 
-// Request interceptor to add token
-   api.interceptors.request.use(
-    (config)=>{
-        const token=localStorage.getItem("token");
-        if(token) 
-        {
-            config.headers['Authoriztion']= `Bearer ${token}`;
-        }
 
-        return config;
-    },
-    (error) => Promise.reject(error),
-    
-   );
-
-   // Response interceptor for error handling
-   api.interceptors.response.use(
-    (response) => response,
-    
-    (error) => {
-        if(error.response) {
-            if(error.response.status === 401){
-                console.log('Unauthorized! Redirect to login?')
+    // Hooks
+    hooks:{
+        beforeRequest:[
+            request => {
+               const token = localStorage.getItem('token');
+               if(token){
+                request.headers.set('Authorization', `Bearer ${token}`);
+               }
             }
-        }
-        return Promise.reject(error);
-    }
-   );
+        ],
+        afterResponse:[
+            (request,response)=>{
+                if(!response.ok){
+                    if(response.status===401)
+                        console.log('Unauthorized! Redirect to login?');
+                }
+                return response;
+            }
+        ],
+    },
+
+    throwHttpErrors : true
+
+});
 
    export default api;

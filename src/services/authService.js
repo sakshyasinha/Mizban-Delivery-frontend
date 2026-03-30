70237c52f5fba80063b694caba79bc438e50d27d
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-03-30 14:20:42 +0430

    remove console.logs

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,9 +3,7 @@ import {handleApiError} from './handleApiError';
 
 export const signup = async(userData) => {
     try{
-        console.log('userData',{...userData});
         const response = await api.post('auth/register', {json: userData}).json();
-        console.log('response:',response);
         return response;
     }catch(error){
        await handleApiError(error);

483a3b29514814044b743f8d8f8d0fb840dd1b10
fataneh-mow <fataneh.mow@gmail.com>
2026-03-30 14:19:16 +0430

    style: made sidebar responsive #31

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,65 +1,73 @@
-import { FaBox } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
+import { FaBox } from "react-icons/fa";
 import { FaTruckMoving } from "react-icons/fa6";
 import { IoPeopleSharp } from "react-icons/io5";
 import { GrAnalytics } from "react-icons/gr";
 import { GiKnifeFork } from "react-icons/gi";
 import { IoMdSettings } from "react-icons/io";
 
+export default function Sidebar({isOpen, setIsOpen}) {
 
+  const navItems = [
+    { key: "dashboard", path: "/", icon: <MdDashboard size={20} />, label: "Dashboard" },
+    { key: "orders", path: "/orders", icon: <FaBox size={18} />, label: "Orders" },
+    { key: "deliveries", path: "/deliveries", icon: <FaTruckMoving size={20} />, label: "Deliveries" },
+    { key: "couriers", path: "/couriers", icon: <IoPeopleSharp size={20} />, label: "Couriers" },
+    { key: "analytics", path: "/analytics", icon: <GrAnalytics size={20} />, label: "Analytics" },
+    { key: "menu-manager", path: "/menu-manager", icon: <GiKnifeFork size={20} />, label: "Menu Manager" },
+    { key: "settings", path: "/settings", icon: <IoMdSettings size={20} />, label: "Settings" },
+  ];
 
-export default function Sidebar() {
   const activeStyle = ({ isActive }) =>
-    isActive ? "bg-orange-600 text-white rounded-lg shadow-orange-100" : "text-gray-700 hover:bg-gray-200 hover:rounded-lg";
+    isActive
+      ? "bg-orange-600 text-white rounded-lg shadow-orange-100"
+      : "text-gray-700 hover:bg-gray-200 hover:rounded-lg";
 
   return (
-    <aside className="w-64 h-screen bg-white border-r border-gray-200 flex flex-col p-4">
+    <>
+      {/* desktop lg */}
+      <aside
+        className={`
+          hidden md:flex flex-col p-4
+          w-64 fixed top-16 left-0 h-[calc(100vh-64px)] bg-white border-r border-gray-200overflow-y-auto mt-1
+        `}
+      >
+        <nav className="flex flex-col gap-2">
+          {navItems.map((item) => (
+            <NavLink key={item.key} to={item.path} className={activeStyle}>
+              <div className="flex items-center gap-3 p-2 rounded-md">
+                {item.icon} {item.label}
+              </div>
+            </NavLink>
+          ))}
+        </nav>
+      </aside>
 
-      <nav className="flex flex-col gap-2">
-        <NavLink to="/" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <MdDashboard size={20}/> Dashboard
-          </div>
-        </NavLink>
-
-        <NavLink to="/orders" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <FaBox size={18} /> Orders
-          </div>
-        </NavLink>
-
-        <NavLink to="/deliveries" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <FaTruckMoving size={20}/> Deliveries
-          </div>
-        </NavLink>
-
-        <NavLink to="/couriers" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <IoPeopleSharp size={20}/> Couriers
-          </div>
-        </NavLink>
-
-        <NavLink to="/analytics" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <GrAnalytics size={20}/> Analytics
-          </div>
-        </NavLink>
-
-        <NavLink to="/menu-manager" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <GiKnifeFork size={20}/> Menu Manager
-          </div>
-        </NavLink>
-
-        <NavLink to="/settings" className={activeStyle}>
-          <div className="flex items-center gap-3 p-2 rounded-md">
-            <IoMdSettings size={20}/> Settings
-          </div>
-        </NavLink>
-
-      </nav>
-    </aside>
+      {/* mobile sm, md */}      
+      <aside
+        className={`
+          md:hidden fixed top-0 left-0 z-40
+          w-64 h-screen bg-white border-r border-gray-200 p-4
+          transform transition-transform duration-300 mt-12
+          ${isOpen ? "translate-x-0" : "-translate-x-full"}
+        `}
+      >
+        <nav className="flex flex-col gap-2 mt-16 overflow-y-auto">
+          {navItems.map((item) => (
+            <NavLink
+              key={item.key}
+              to={item.path}
+              className={activeStyle}
+              onClick={() => setIsOpen(false)}
+            >
+              <div className="flex items-center gap-3 p-2 rounded-md">
+                {item.icon} {item.label}
+              </div>
+            </NavLink>
+          ))}
+        </nav>
+      </aside>
+    </>
   );
 }

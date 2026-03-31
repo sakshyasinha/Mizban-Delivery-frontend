b08c47300256dc22a00abe8dd646debbfc914b1b
Shukria <shukriasul5@gmail.com>
2026-03-31 14:33:47 +0430

    feat: set up requests in orderService file

---
+++ src/services/orderService.js
@@ -0,0 +1,18 @@
+import api from "./api"
+
+export const createNewOrder = async (orderData) => {
+    const response = await api.post("/orders", { json: orderData }).json()
+    return response
+}
+export const updatedOrder = async (orderId, updatedOrderData) => {
+    const response = await api.put(`/orders/${orderId}`, { json: updatedOrderData })
+    return response
+}
+export const cancelOrder = async (orderId, cancelReason) => {
+    const response = await api.patch(`/orders/${orderId}/cancel`, { json: cancelReason })
+    return response
+}
+export const markOrderDelivered = async (orderId) => {
+    const response = await api.patch(`/orders/${orderId}/deliver`)
+    return response
+}
